db0dd964f8805815253ed41d347ca22ddc4c04b7
aigeram <aigerimavazbekovas@gmail.com>
2022-11-17 20:38:58 +0600

    make variables

---
+++ lesson2_practice.dart
@@ -0,0 +1,34 @@
+void main() {
+  String name = 'aigerim';
+  int age = 19;
+  double weight = 50.6;
+  bool isMan = false;
+
+  List array = [9789, '967967'];
+
+  Map human = {
+    "name": "aigerim",
+    "age": 19,
+  };
+
+  dynamic g = 'gfdgfd';
+  g=324;
+  g=false;
+ 
+  var a= 234234;
+  a=4234234;
+
+  final b;
+  b=235235;
+
+  
+  const c = 4444;
+
+var heart_symbol = '\u2665';
+var laugh_symbol = '\u{1f600}';
+print(heart_symbol);
+print(laugh_symbol);
+ 
+
+ 
+}
