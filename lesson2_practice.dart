89ce8ddbc4082ddf928559d184a2c4243d44f9f2
aigeram <118278771+aigeram@users.noreply.github.com>
2022-11-19 13:50:27 +0600

    lesson2_practice.dart

--- lesson2_practice.dart
+++ lesson2_practice.dart
@@ -1,34 +1,9 @@
-void main() {
-  String name = 'aigerim';
-  int age = 19;
-  double weight = 50.6;
-  bool isMan = false;
+void main(){
+  String name = 'keu';
+  int age =50;
+  double weight = 90.9;
+  bool isHome = false;
 
-  List array = [9789, '967967'];
-
-  Map human = {
-    "name": "aigerim",
-    "age": 19,
-  };
-
-  dynamic g = 'gfdgfd';
-  g=324;
-  g=false;
- 
-  var a= 234234;
-  a=4234234;
-
-  final b;
-  b=235235;
-
+ print('keu');
   
-  const c = 4444;
-
-var heart_symbol = '\u2665';
-var laugh_symbol = '\u{1f600}';
-print(heart_symbol);
-print(laugh_symbol);
- 
-
- 
 }
